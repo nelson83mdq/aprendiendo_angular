4f989325a750691270c41714249b3a90e9855b1d
Nelson Damian Brizuela <nelson83mdq@gmail.com>
2023-05-07 17:48:31 -0300

    adding routing and a simple menu

---
+++ src/app/library/book.ts
@@ -0,0 +1,10 @@
+export class Book {
+    constructor(
+        public name: String,
+        public published: Date,
+        public author: String,
+        public editorial: String,
+        public review: Number,
+    ){}
+
+}
--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from './book';
 
 @Component({
   selector: 'app-library',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./library.component.css']
 })
 export class LibraryComponent {
+  public bookList: Array<Book>;
 
+  constructor(){
+    this.bookList = [
+      new Book('El Viejo y el Mar',new Date('1980/05/23'),'Hemingway', 'P&G',100),
+      new Book('Mas respeto que soy tu madre',new Date('2005/07/02'),'Casciari', 'Orsai',500),
+      new Book('El Aleph',new Date('1980/05/1'),'Borges', 'Editorial Buenos Aires',200),
+      new Book('jueguetes Perdidos',new Date('1989/12/3'),'Cortazar', 'Universidad Nacional',150)
+    ]
+  }
 }
